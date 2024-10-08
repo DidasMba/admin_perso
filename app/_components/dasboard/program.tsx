2cd8d9aef7493b25ae83a16914f01a1b38bc81e8
DidasMba <daniel.mbarushi@gmail.com>
2024-10-08 18:50:07 +0300

    add in pro

--- app/_components/dasboard/program.tsx
+++ app/_components/dasboard/program.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { CiDeliveryTruck } from 'react-icons/ci'
 
 const Program = () => {
   return (
-    <div>Program</div>
+   <main>
+     <div>
+            <div className=''>Program</div>
+            <div className=''>Program</div>
+            <div className=''>Program</div>
+    </div>
+
+   </main>
+
+   
   )
 }
 
