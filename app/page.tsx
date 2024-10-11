1e23bd49fa97ddcd4dfdb6021500d9902e344bc5
gracebir <grace.devolop@gmail.com>
2024-10-11 07:20:53 +0200

    authentication and authorization

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
-  return (
-   <main>
-    <h1>Welcome to my home page</h1>
-   </main>
-  );
+/** @format */
+
+import { getCcookie } from "@/lib";
+import LoginForm from "./_components/signin/SignForm";
+
+export default async function Home() {
+    const token = await getCcookie();
+    return (
+        <main className='min-h-svh w-full flex items-center bg-gray-100 px-6 lg:px-0'>
+            <LoginForm isToken={token!} />
+        </main>
+    );
 }
