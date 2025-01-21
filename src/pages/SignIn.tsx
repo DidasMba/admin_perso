7ebccbaaa24116e459383f767160879d40c0b657
gracebir <grace.devolop@gmail.com>
2025-01-21 20:20:57 +0200

    login functionality

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,7 +3,7 @@ import LoginForm from "../components/signin/SignForm";
 
 const SignIn: React.FC = () => {
   return (
-    <div className="w-full min-h-svh font-montserrat flex items-center">
+    <div className="w-full min-h-svh px-6 bg-gray-100 font-montserrat flex items-center">
       <LoginForm />
     </div>
   );
