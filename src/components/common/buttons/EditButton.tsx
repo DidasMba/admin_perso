aa0badcb50cb0ebc3adde77144cbc8793a5ae572
didasmba <daniel.mbarushi@gmail.com>
2025-01-27 22:57:14 +0300

    editbutton

--- src/components/common/buttons/EditButton.tsx
+++ src/components/common/buttons/EditButton.tsx
@@ -2,17 +2,18 @@ import React from "react";
 
 interface EditButtonProps {
   onClick: () => void; // Fonction à appeler lors du clic
-  isEditing: boolean; // État pour savoir si on est en mode édition
+  // isEditing: boolean; // État pour savoir si on est en mode édition
 }
 
-const EditButton: React.FC<EditButtonProps> = ({ onClick, isEditing }) => {
+const EditButton: React.FC<EditButtonProps> = ({ onClick }) => {
   return (
     <div className="mt-4 flex justify-end p-6">
       <button
         onClick={onClick} // Appelle la fonction passée en prop
         className="bg-customBlue text-white p-2 rounded-lg flex items-center gap-2 hover:bg-[#2d3748] cursor-pointer"
       >
-        {isEditing ? "Save Changes" : "Edit Profile"}
+        {/* {isEditing ? "Enregistrer Changement" : "Modifier Profile"} */}
+        Modifier Profile
       </button>
     </div>
   );
