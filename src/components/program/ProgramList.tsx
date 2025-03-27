bfc327cc0f3b8589f8243055f0ce3b2154a1e599
didasmba <daniel.mbarushi@gmail.com>
2025-03-27 20:39:57 +0300

    title programe line

--- src/components/program/ProgramList.tsx
+++ src/components/program/ProgramList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Heading from "../common/Heading";
 import { Program } from "../types/program";
 
 type Props = { programs: Program[] };
@@ -6,7 +7,7 @@ type Props = { programs: Program[] };
 const ProgramList: React.FC<Props> = ({ programs }) => {
   return (
     <div className="bg-white p-4 rounded-lg shadow-md">
-      <h2 className="text-lg font-semibold mb-3">Vos Programmes</h2>
+      <Heading text="Mes Programmes" />
       <ul className="space-y-3">
         {programs.map((program) => (
           <li key={program.id} className="p-3 bg-gray-100 rounded-md">
